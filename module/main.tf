fd6a1dcc9779fde134435c4e704d24462186dccd
Monika <monika.chitimalla@gmail.com>
2023-05-10 10:54:17 +0100

    Iterative Model

--- module/main.tf
+++ module/main.tf
@@ -1,22 +1,35 @@
-data "aws_ami" "centosimage"{
-  owners = ["973714476881"]
-  most_recent = true
-  name_regex = "Centos-8-DevOps-Practice"
-}
+
 
 resource "aws_instance" "instance" {
   ami           = data.aws_ami.centosimage.image_id
   instance_type = var.instance_type
-
+  vpc_security_group_ids = [data.aws_security_group.AllowAll.id]
   tags = {
     Name = var.component_name
   }
 }
 resource "aws_route53_record" "record" {
   depends_on = [aws_instance.instance]
-    name    = "${var.component_name}.eroboshop.online"
+  name    = "${var.component_name}.eroboshop.online"
   type    = "A"
   zone_id = "Z02749461DPP5346XTQCJ"
   ttl = 30
   records =[aws_instance.instance.private_ip]
+}
+resource "null_resource" "Provisioner" {
+  depends_on = [aws_route53_record.record]
+  provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      user     = "centos"
+      password = "DevOps321"
+      host     = aws_instance.instance.private_ip
+    }
+    inline=[
+      "rm -rf RoboshopAutomation",
+      "git clone https://github.com/mchitimalla/RoboshopAutomation.git",
+      "cd RoboshopAutomation",
+      "sudo bash ${var.component_name}.sh ${var.password}"
+    ]
+  }
 }
